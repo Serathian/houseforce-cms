3e77700f1563ee6fc8102e94df448422fda61598
Serathian <jake.a.reddy@gmail.com>
2024-06-10 22:46:25 +0300

    clean up content area

--- client/src/lib/components/content-area/contentArea.svelte
+++ client/src/lib/components/content-area/contentArea.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
 	import { getDisplayOption, getImageStyle } from '@/utils/styleHelper';
-	import RichTextBlock from '../blocks/richTextBlock.svelte';
-	import StrapiImageBlock from '../blocks/strapiImageBlock.svelte';
+	import RichTextBlock from '../blocks/RichTextBlock.svelte';
+	import StrapiImageBlock from '../blocks/StrapiImageBlock.svelte';
 
+	// TODO: type this
+	//item.Media.data is an array?
 	export let data;
 </script>
 
-<div class="grid gap-6 grid-cols-12">
+<div class="grid grid-cols-12 gap-6">
 	{#each data as item}
 		{#if item.__component === 'blocks.text-block'}
 			<RichTextBlock data={item.Text} cssClass={getDisplayOption(item.DisplayOption)} />
 		{:else if item.__component === 'blocks.image-block'}
-			<StrapiImageBlock data={item.Media} cssClass={getDisplayOption(item.DisplayOption) + " " + getImageStyle(item.Style)} />
+			<StrapiImageBlock
+				imageData={item.Media.data[0]}
+				cssClass={getDisplayOption(item.DisplayOption) + ' ' + getImageStyle(item.Style)}
+			/>
 		{/if}
 	{/each}
 </div>
