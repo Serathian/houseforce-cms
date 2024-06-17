19ad9f354f1e4a5d8904f2bf1c20009816237189
Serathian <jake.a.reddy@gmail.com>
2024-06-17 20:58:32 +0300

    hero subdomain layout

--- client/src/lib/components/hero/Hero.svelte
+++ client/src/lib/components/hero/Hero.svelte
@@ -1,27 +1,40 @@
 <script lang="ts">
-	import { currentTheme } from '@/stores';
-	import Brand from '../blocks/Brand.svelte';
-	import { Domains } from '@/utils/constants';
+  import { currentTheme } from "@/stores";
+  import Brand from "../blocks/Brand.svelte";
+  import { Domains } from "@/utils/constants";
 
-	let heroText: string;
-	let isHomecare: boolean;
-	let isRenovation: boolean;
-	currentTheme.subscribe((value) => {
-		heroText = value.heroText;
-		isHomecare = value.currentDomain.domain == Domains.Homecare;
-		isRenovation = value.currentDomain.domain == Domains.Renovation;
-	});
+  let heroText: string;
+  let isHomecare: boolean;
+  let isRenovation: boolean;
+  currentTheme.subscribe((value) => {
+    heroText = value.heroText;
+    isHomecare = value.currentDomain.domain == Domains.Homecare;
+    isRenovation = value.currentDomain.domain == Domains.Renovation;
+  });
 
-	const activeElementCss = 'py-1 px-3 m-1 bg-white rounded-full shadow-md font-bold';
-	const inactiveElementCss = 'py-1 px-3 m-1 rounded-full font-bold';
+  const activeElementCss =
+    "py-1 px-3 m-1 bg-white rounded-full shadow-md font-bold";
+  const inactiveElementCss = "py-1 px-3 m-1 rounded-full font-bold";
 </script>
 
-<header class="bg-muted flex h-[500px] flex-col items-center justify-center gap-3">
-	<Brand />
-	<div class="bg-secondary-foreground flex w-max gap-6 rounded-full">
-		<a href="/homecare" class={isHomecare ? activeElementCss : inactiveElementCss}> Homecare </a>
-		<a href="/renovation" class={isRenovation ? activeElementCss : inactiveElementCss}>
-			Renovation
-		</a>
-	</div>
+<header
+  class="bg-muted flex h-[500px] flex-col items-center justify-center gap-3"
+>
+  <Brand />
+  {#if isHomecare || isRenovation}
+    <div class="bg-secondary-foreground flex w-max gap-6 rounded-full">
+      <a
+        href="/homecare"
+        class={isHomecare ? activeElementCss : inactiveElementCss}
+      >
+        Homecare
+      </a>
+      <a
+        href="/renovation"
+        class={isRenovation ? activeElementCss : inactiveElementCss}
+      >
+        Renovation
+      </a>
+    </div>
+  {/if}
 </header>
