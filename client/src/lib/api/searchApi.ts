91063ab3dd0ed5f4d0007bc2f2ab9106b91d0506
Serathian <jake.a.reddy@gmail.com>
2024-06-17 22:23:12 +0300

    add types to meilisearch

--- client/src/lib/api/searchApi.ts
+++ client/src/lib/api/searchApi.ts
@@ -1,20 +1,61 @@
 import { PUBLIC_SEARCH_BASE_URL } from "$env/static/public";
 
-export const Search = async (query: string) => {
+interface MeilisearchParameters {
+  q?: string;
+  offset?: number;
+  limit?: number;
+  hitsPerPage?: number;
+  page?: number;
+  filter?: string;
+  facets?: string[];
+  attributesToRetrieve?: string[];
+  attributesToCrop?: string[];
+  cropLength?: number | undefined;
+  attributesToHighlight?: string[];
+  highlightPreTag?: string;
+  highlightPostTag?: string;
+  showMatchesPosition?: boolean;
+  sort?: string[];
+  matchingStrategy?: string;
+  showRankingScore?: boolean;
+  attributesToSearchOn?: string[];
+  hybrid?: Object;
+  vector?: number[];
+}
+
+interface MeilisearchResult<T> {
+  hits: T[];
+  offest: number;
+  limit: number;
+  estimatedTotalHits: number;
+  totalHits: number;
+  totalPages: number;
+  hitsPerPage: number;
+  page: number;
+  facetDistribution: Object;
+  facetStats: Object;
+  processingTimeMs: Number;
+  query: string;
+}
+
+export const Search = async (query: string, fetcher: typeof fetch = fetch) => {
   const url = PUBLIC_SEARCH_BASE_URL + "/indexes/content/search";
 
-  const response = await fetch(url, {
+  const searchParams: MeilisearchParameters = {
+    q: query,
+  };
+
+  const response = await fetcher(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      q: query,
-    }),
+    body: JSON.stringify(searchParams),
   });
 
   if (!response.ok) {
     throw new Error("Error in search");
   }
-  return response.json();
+  const result: MeilisearchResult<Object> = await response.json();
+  return result;
 };
