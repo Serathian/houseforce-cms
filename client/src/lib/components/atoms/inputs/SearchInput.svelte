65ec12aa3d7174737a4e3b42aea4c4eb5356fb3e
Serathian <jake.a.reddy@gmail.com>
2024-06-17 22:23:29 +0300

    add searchInput with redirect

---
+++ client/src/lib/components/atoms/inputs/SearchInput.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Input } from "$lib/components/ui/input";
+  import { goto, replaceState } from "$app/navigation";
+  import Search from "lucide-svelte/icons/search";
+</script>
+
+<div class="relative">
+  <Search class="text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4" />
+  <Input
+    on:keydown={(e) => {
+      if (e.key !== "Enter") return;
+      const input = e.currentTarget;
+      const query = input.value;
+      goto(`/search?query=` + query, { replaceState: true });
+    }}
+    type="search"
+    placeholder="Search..."
+    class="bg-background focus-visible:ring-primary w-full rounded-lg pl-8"
+  />
+</div>
