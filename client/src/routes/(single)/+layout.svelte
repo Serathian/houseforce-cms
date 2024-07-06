dedf6b92f0773c3408f640b9436fc571ebdc4347
Serathian <jake.a.reddy@gmail.com>
2024-07-06 13:15:21 +0300

    add contact bubble to layout

--- client/src/routes/(single)/+layout.svelte
+++ client/src/routes/(single)/+layout.svelte
@@ -2,15 +2,17 @@
   import Navigation from "$lib/components/navigation/Navigation.svelte";
   import Hero from "$lib/components/hero/Hero.svelte";
   import Footer from "@/components/footer/Footer.svelte";
+  import ContactBubble from "@/components/contact/ContactBubble.svelte";
 </script>
 
 <div class="relative">
   <Navigation />
   <Hero />
 
-  <div class="container grid gap-3 pt-6">
+  <div class="container grid gap-3 pt-6 pb-20">
     <slot />
   </div>
 
+  <ContactBubble />
   <Footer />
 </div>
